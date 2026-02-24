a370580c8b9fe29634efb6b7218e733dbf7a921f
Cristian Fernando Dávila López <cristianfernandodavilalopez@gmail.com>
2026-02-23 22:00:17 -0500

    added evaluator to the project

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -4,6 +4,7 @@ import json
 import os
 import requests
 from pypdf import PdfReader
+from pydantic import BaseModel
 import gradio as gr
 
 
@@ -72,6 +73,10 @@ record_unknown_question_json = {
 tools = [{"type": "function", "function": record_user_details_json},
         {"type": "function", "function": record_unknown_question_json}]
 
+# Create a Pydantic model for the Evaluation
+class Evaluation(BaseModel):
+    is_acceptable: bool
+    feedback: str
 
 class Me:
 
@@ -90,6 +95,16 @@ class Me:
                 self.linkedin += text
         with open("me/summary.txt", "r", encoding="utf-8") as f:
             self.summary = f.read()
+        
+        self.evaluator_system_prompt = f"You are an evaluator that decides whether a response to a question is acceptable. \
+            You are provided with a conversation between a User and an Agent. Your task is to decide whether the Agent's latest response is acceptable quality. \
+            The Agent is playing the role of {self.name} and is representing {self.name} on their website. \
+            The Agent has been instructed to be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+            The Agent has been provided with context on {self.name} in the form of their summary and LinkedIn details. Here's the information:"
+
+        self.evaluator_system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
+        self.evaluator_system_prompt += f"With this context, please evaluate the latest response, replying with whether the response is acceptable and your feedback."
+
 
 
     def handle_tool_call(self, tool_calls):
@@ -105,20 +120,47 @@ class Me:
     
     def system_prompt(self):
         system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
-particularly questions related to {self.name}'s career, background, skills and experience. \
-Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
-You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
-Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
-Always respond in the same language in which the user asked the question. \
-If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
-If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
+            particularly questions related to {self.name}'s career, background, skills and experience. \
+            Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
+            You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
+            Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
+            Always respond in the same language in which the user asked the question. \
+            If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+            If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their email and record it using your record_user_details tool. "
 
         system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
         system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
         return system_prompt
     
+    
+    def evaluator_user_prompt(self, reply, message, history):
+        user_prompt = f"Here's the conversation between the User and the Agent: \n\n{history}\n\n"
+        user_prompt += f"Here's the latest message from the User: \n\n{message}\n\n"
+        user_prompt += f"Here's the latest response from the Agent: \n\n{reply}\n\n"
+        user_prompt += "Please evaluate the response, replying with whether it is acceptable and your feedback."
+        return user_prompt
+    
+    def evaluate(self, reply, message, history) -> Evaluation:
+        messages = [
+            {"role": "system", "content": self.evaluator_system_prompt},
+            {"role": "user", "content": self.evaluator_user_prompt(reply, message, history)},
+        ]
+        response = self.gemini.beta.chat.completions.parse(
+            model="gemini-2.0-flash",
+            messages=messages,
+            response_format=Evaluation,
+        )
+        return response.choices[0].message.parsed
+    
+    def rerun(self, reply, message, history, feedback):
+        updated_system_prompt = self.system_prompt + "\n\n## Previous answer rejected\nYou just tried to reply, but the quality control rejected your reply\n"
+        updated_system_prompt += f"## Your attempted answer:\n{reply}\n\n"
+        updated_system_prompt += f"## Reason for rejection:\n{feedback}\n\n"
+        messages = [{"role": "system", "content": updated_system_prompt}] + history + [{"role": "user", "content": message}]
+        response = self.gemini.chat.completions.create(model="gemini-2.5-flash", messages=messages)
+        return response.choices[0].message.content
+    
     def chat(self, message, history):
-        
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
